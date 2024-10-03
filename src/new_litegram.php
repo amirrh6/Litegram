194fec26c3959a84e97c3a83437fc85979660f95
amirrh6 <amirrezahh6@gmail.com>
2024-10-03 14:37:40 +0330

    Completed some of the classes

--- src/new_litegram.php
+++ src/new_litegram.php
@@ -3,9 +3,11 @@
 // TODO: Use a namespace
 
 define('LITEGRAM_BOT_API_VERSION', '7.10');
-define('LITEGRAM_VERSION', '0.4.0');
+define('LITEGRAM_VERSION', '0.5.0');
 
 // TODO: Consider the need for setting up arrays in the constructors
+
+// -------------------------------------------------------------------
 
 /**
  * This object represents an incoming update.
@@ -979,11 +981,41 @@ class Message
 }
 
 /**
- * TODO:
+ * Describes the connection of the bot with a business account.
  */
 #[\AllowDynamicProperties]
 class BusinessConnection
 {
+    /**
+     * Unique identifier of the business connection
+     */
+    public string $id;
+
+    /**
+     * Business account user that created the business connection
+     */
+    public User $user;
+
+    /**
+     * Identifier of a private chat with the user who created the business connection. This number may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a 64-bit integer or double-precision float type are safe for storing this identifier.
+     */
+    public int $user_chat_id;
+
+    /**
+     * Date the connection was established in Unix time
+     */
+    public int $date;
+
+    /**
+     * True, if the bot can act on behalf of the business account in chats that were active in the last 24 hours
+     */
+    public bool $can_reply;
+
+    /**
+     * True, if the connection is active
+     */
+    public bool $is_enabled;
+
     public function __construct($init_data)
     {
         $arr = get_object_vars($init_data);
@@ -993,15 +1025,35 @@ class BusinessConnection
                 $this->$key = $init_data->$key;
             }
         }
+
+        if (property_exists($init_data, 'user')) {
+            $this->user = new User($init_data->user);
+        }
     }
 }
 
 /**
- * TODO:
+ * This object is received when messages are deleted from a connected business account.
  */
 #[\AllowDynamicProperties]
 class BusinessMessageDeleted
 {
+    /**
+     * Unique identifier of the business connection
+     */
+    public string $business_connection_id;
+
+    /**
+     * Information about a chat in the business account. The bot may not have access to the chat or the corresponding user.
+     */
+    public Chat $chat;
+
+    /**
+     * The list of identifiers of deleted messages in the chat of the business account
+     * @var array<int>
+     */
+    public array $message_ids;
+
     public function __construct($init_data)
     {
         $arr = get_object_vars($init_data);
@@ -1010,6 +1062,10 @@ class BusinessMessageDeleted
             } else {
                 $this->$key = $init_data->$key;
             }
+        }
+
+        if (property_exists($init_data, 'chat')) {
+            $this->chat = new Chat($init_data->chat);
         }
     }
 }
@@ -1327,11 +1383,21 @@ class PreCheckoutQuery
 }
 
 /**
- * TODO:
+ * This object contains information about a paid media purchase.
  */
 #[\AllowDynamicProperties]
 class PaidMediaPurchased
 {
+    /**
+     * User who purchased the media
+     */
+    public User $from;
+
+    /**
+     * Bot-specified paid media payload
+     */
+    public string $paid_media_payload;
+
     public function __construct($init_data)
     {
         $arr = get_object_vars($init_data);
@@ -1340,6 +1406,10 @@ class PaidMediaPurchased
             } else {
                 $this->$key = $init_data->$key;
             }
+        }
+
+        if (property_exists($init_data, 'from')) {
+            $this->from = new User($init_data->from);
         }
     }
 }
