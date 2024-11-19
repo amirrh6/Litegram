9f293783acb7ea09b3c3d4a88af796e4780c3268
amirrh6 <amirrezahh6@gmail.com>
2024-11-19 13:03:33 +0330

    Complete InlineKeyboardButton class

--- src/primary_types.php
+++ src/primary_types.php
@@ -3115,7 +3115,43 @@ class InlineKeyboardButton extends CustomJsonSerialization
      */
     public ?string $callback_data = null;
 
-    // TODO ...
+    /**
+     * Optional. Description of the Web App that will be launched when the user presses the button. The Web App will be able to send an arbitrary message on behalf of the user using the method answerWebAppQuery. Available only in private chats between a user and the bot. Not supported for messages sent on behalf of a Telegram Business account.
+     */
+    public ?WebAppInfo $web_app = null;
+
+    /**
+     * Optional. An HTTPS URL used to automatically authorize the user. Can be used as a replacement for the Telegram Login Widget.
+     */
+    public ?LoginUrl $login_url = null;
+
+    /**
+     * Optional. If set, pressing the button will prompt the user to select one of their chats, open that chat and insert the bot's username and the specified inline query in the input field. May be empty, in which case just the bot's username will be inserted. Not supported for messages sent on behalf of a Telegram Business account.
+     */
+    public ?string $switch_inline_query = null;
+
+    /**
+     * Optional. If set, pressing the button will insert the bot's username and the specified inline query in the current chat's input field. May be empty, in which case only the bot's username will be inserted.
+     * This offers a quick way for the user to open your bot in inline mode in the same chat - good for selecting something from multiple options. Not supported in channels and for messages sent on behalf of a Telegram Business account.
+     */
+    public ?string $switch_inline_query_current_chat = null;
+
+    /**
+     * Optional. If set, pressing the button will prompt the user to select one of their chats of the specified type, open that chat and insert the bot's username and the specified inline query in the input field. Not supported for messages sent on behalf of a Telegram Business account.
+     */
+    public ?SwitchInlineQueryChosenChat $switch_inline_query_chosen_chat = null;
+
+    /**
+     * Optional. Description of the game that will be launched when the user presses the button.
+     * NOTE: This type of button must always be the first button in the first row.
+     */
+    public ?CallbackGame $callback_game = null;
+
+    /**
+     * Optional. Specify True, to send a Pay button. Substrings “⭐” and “XTR” in the buttons's text will be replaced with a Telegram Star icon.
+     * NOTE: This type of button must always be the first button in the first row and can only be used in invoice messages.
+     */
+    public ?bool $pay = null;
 
     public function __construct(
         string $text,
